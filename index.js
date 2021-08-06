4df960ddc59283f98ce63e8d7d9a71cf8314b671
EBLLIS <88104016+EBLLIS@users.noreply.github.com>
2021-08-07 03:34:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODE0NjA4NzA3NDEyMjk1Nzgw.YDgVeg.HvMJ4jRnJPfxjBa9QdmdcnN1pvE');
+client.login('ODczMzA0NzQ3NjAyNDg5MzY0.YQ2ebQ._Nami4AcDJm43oSylDa2GLlWPz0');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
